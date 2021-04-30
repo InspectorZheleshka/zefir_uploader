0cc3dfd58cc4d1b0fd2131d8ee2dbf0d223ee6e0
InspectorJeleshka <->
2021-04-30 13:46:02 +0300

    Albums info mutations

--- data/data_layer.py
+++ data/data_layer.py
@@ -1,12 +1,16 @@
 from api import uploader_api
 from local_storage import local_storage
 
+from os.path import join
+
 api = uploader_api.Api()
 api.login()
 
 
-def get_albums():
-    return get_albums_for_cat()
+def get_albums_remote():
+    albums = get_albums_for_cat()
+    albums = flatten_remote_albums(albums)
+    return albums
 
 
 def get_albums_for_cat(parent_cat=None):
@@ -23,16 +27,63 @@ def get_albums_for_cat(parent_cat=None):
             del albums[pos]
 
     for alb in albums:
-        alb['__content__'] = get_albums_for_cat(alb['id'])
+        if alb['nb_categories'] > 0:
+            alb['__content__'] = get_albums_for_cat(alb['id'])
 
-        if len(alb['__content__']) == 0:
-            del alb['__content__']
+            if len(alb['__content__']) == 0:
+                del alb['__content__']
 
     return albums
 
 
-def get_albums_to_upload():
-    return local_storage.get_local_albums()
+def flatten_remote_albums(albums, parent=None):
+    res = []
+
+    for album in albums:
+        content = album.get('__content__', None)
+
+        if '__content__' in albums:
+            del album['__content__']
+
+        if parent is not None:
+            album['parent_id'] = parent
+
+        res.append(album)
+
+        if content is not None:
+            content = flatten_remote_albums(content, album['id'])
+            res.extend(content)
+
+    return res
+
+
+def get_albums_local():
+    albs = local_storage.get_local_albums()
+    albs = flatten_local_albums(albs)
+    albs = filter_local_albs(albs)
+    return albs
+
+
+def flatten_local_albums(albums, title=local_storage.storage_path, path=None):
+    res = []
+    alb = dict()
+    alb['parent'] = path
+    alb['title'] = title
+
+    for alb_title in albums:
+        if alb_title == '__content__':
+            alb['__content__'] = albums[alb_title]
+        else:
+            al_path = join(path, title) if path is not None else title
+            res.extend(flatten_local_albums(albums[alb_title], title=alb_title, path=al_path))
+
+    res.append(alb)
+
+    return res
+
+
+def filter_local_albs(albums):
+    return [album for album in albums if len(album['__content__']) > 0]
 
 
 def create_album(name):
--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from data import data_layer as storage
 
 
 def get_local_albums():
-    return storage.get_albums_to_upload()
+    return storage.get_albums_local()
 
 
 def get_remote_albums():
-    return storage.get_albums()
+    return storage.get_albums_remote()
 
 
 def main():
     local_albs = get_local_albums()
-    remote_albums = storage.get_albums()
+    remote_albums = get_remote_albums()
 
     print(local_albs)
     print(remote_albums)
+
+    print([alb['title'] for alb in local_albs])
+    print([alb['name'] for alb in remote_albums])
 
 
 if __name__ == '__main__':
